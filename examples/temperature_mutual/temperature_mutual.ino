2e6b830d4ab7f9ec2d6ffa3f35ccf19c5b8cfea8
MX682X <mx682x@gmx.de>
2023-09-15 17:21:21 +0200

    fix types

--- examples/temperature_mutual/temperature_mutual.ino
+++ examples/temperature_mutual/temperature_mutual.ino
@@ -29,7 +29,7 @@ void loop() {
 }
 
 // callback that is called by ptc_process at different points to ease user interaction
-void ptc_event_callback(const uint8_t eventType, cap_sensor_t* node) {
+void ptc_event_callback(const ptc_cb_event_t eventType, cap_sensor_t* node) {
   if (PTC_CB_EVENT_TOUCH_DETECT == eventType) {
     MySerial.print("node touched:");
     MySerial.println(ptc_get_node_id(node));
